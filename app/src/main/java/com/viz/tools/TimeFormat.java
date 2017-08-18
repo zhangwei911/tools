90fe58eaf517def9fa97b9babf32c0a8ad9035f7
wellchang <656849170@qq.com>
2017-08-18 10:48:20 +0800

    时间格式化添加时区

--- app/src/main/java/com/viz/tools/TimeFormat.java
+++ app/src/main/java/com/viz/tools/TimeFormat.java
@@ -4,45 +4,73 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Created by viz on 2014/11/25.
  */
 public class TimeFormat {
+    public static final String TIMEZONE_SHANGHAI = "Asia/Shanghai";
+
     /**
      * 将字符串数据转化为毫秒数
-     * @param dateTime 格式化时间字符串
+     *
+     * @param dateTime       格式化时间字符串
      * @param dateTimeFormat 时间格式
      * @return 毫秒数
      * @throws ParseException
      */
     public static long getMS(String dateTime, String dateTimeFormat) throws ParseException {
+        return getMS(dateTime, dateTimeFormat, null);
+    }
+
+    public static long getMS(String dateTime, String dateTimeFormat, String timeZone) throws ParseException {
         Calendar c = Calendar.getInstance();
-        c.setTime(new SimpleDateFormat(dateTimeFormat).parse(dateTime));
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateTimeFormat);
+        TimeZone tz = TextUtils.isEmpty(timeZone) ? TimeZone.getDefault() : TimeZone.getTimeZone(timeZone);
+        simpleDateFormat.setTimeZone(tz);
+        c.setTime(simpleDateFormat.parse(dateTime));
         return c.getTimeInMillis();
     }
 
     /**
      * 将毫秒数转化为格式化时间
-     * @param msTime 毫秒
+     *
+     * @param msTime         毫秒
      * @param dateTimeFormat 时间格式
      * @return 格式化时间
      */
     public static String getDateFormatTime(String msTime, String dateTimeFormat) {
+        return getDateFormatTime(msTime, dateTimeFormat, null);
+    }
+
+    public static String getDateFormatTime(String msTime, String dateTimeFormat, String timeZone) {
         SimpleDateFormat sdf = new SimpleDateFormat(dateTimeFormat);
+        TimeZone tz = TextUtils.isEmpty(timeZone) ? TimeZone.getDefault() : TimeZone.getTimeZone(timeZone);
+        sdf.setTimeZone(tz);
         return sdf.format(Long.parseLong(msTime));
     }
 
     /**
      * 转换时间格式
-     * @param dateTime 格式化时间字符串
+     *
+     * @param dateTime     格式化时间字符串
      * @param originFormat 源时间格式
-     * @param newFormat 新时间格式
+     * @param newFormat    新时间格式
      * @return
      */
     public static String changeFormat(String dateTime, String originFormat, String newFormat) {
         try {
             return getDateFormatTime(getMS(dateTime, originFormat) + "", newFormat);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return dateTime;
+        }
+    }
+
+    public static String changeFormat(String dateTime, String originFormat, String newFormat, String oldTimeZone, String newTimeZone) {
+        try {
+            return getDateFormatTime(getMS(dateTime, originFormat, oldTimeZone) + "", newFormat, newTimeZone);
         } catch (ParseException e) {
             e.printStackTrace();
             return dateTime;
