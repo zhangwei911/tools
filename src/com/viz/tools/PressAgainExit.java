c3f7bd037a35173123c3b875ca4404b6cd80f592
张维 <zhangwei>
2018-12-26 14:16:33 +0800

    1.增加quit函数

--- src/com/viz/tools/PressAgainExit.java
+++ src/com/viz/tools/PressAgainExit.java
@@ -29,6 +29,7 @@ public class PressAgainExit {
      */
     public static void pressAgainExit(Context context, boolean isKillProcess) {
         if (exit.isExit()) {
+            exit.quit();
             if (onPressAgainExitListener == null) {
                 if (isKillProcess) {
                     android.os.Process.killProcess(android.os.Process.myPid());
@@ -54,6 +55,7 @@ public class PressAgainExit {
 
 
     static class Exit {
+        private HandlerThread thread = null;
         private boolean isExit = false;
         private final Runnable task = new Runnable() {
             @Override
@@ -65,7 +67,7 @@ public class PressAgainExit {
 
         public void doExitInOneSecond() {
             isExit = true;
-            HandlerThread thread = new HandlerThread("doTask");
+            thread = new HandlerThread("doTask");
             thread.start();
             new Handler(thread.getLooper()).postDelayed(task, 2000);
         }
@@ -75,6 +77,9 @@ public class PressAgainExit {
             return isExit;
         }
 
+        public boolean quit() {
+            return thread.quit();
+        }
 
         public void setExit(boolean isExit) {
             this.isExit = isExit;
